fdf1587a3c49297ce2f321e8c72b47a3f0797300
jony166091 <40674772+jony166091@users.noreply.github.com>
2021-05-31 06:48:16 +0600

    Add files via upload

---
+++ Alexa.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices') 
+engine.setProperty('voice',voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait() 
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+          print('Listening ..... ')
+          voice = listener.listen(source)
+          command = listener.recognize_google(voice)
+        if 'Alexa' in command:
+            print(command)
+    except:
+        pass
+        return command
+   
